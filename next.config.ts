486246b59aa6d8c1afd2b101b5061edc79e70ff1
khj809 <onsealeatang@gmail.com>
2025-11-25 22:57:30 +0900

    fix nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/ailoy-assistant-ui-example",
+  assetPrefix: "/ailoy-assistant-ui-example/",
 };
 
 export default nextConfig;
